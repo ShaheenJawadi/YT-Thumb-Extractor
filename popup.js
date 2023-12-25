d464f0ffaab0b52bcd18eb331c96977b688457ff
shaheenj <jawadishaheen@gmail.com>
2023-12-25 05:52:45 +0100

    download

--- popup.js
+++ popup.js
@@ -2,6 +2,33 @@
 
 var resolution='mqdefault' , vId   ;
 document.addEventListener('DOMContentLoaded',  function () {
+
+    document.getElementById('selectRes').addEventListener('change', function() {
+        resolution=this.value;   
+    });
+
+
+    
+    document.getElementById('downloadImage').addEventListener('click', function () {
+        
+        var imageUrl = 'https://i.ytimg.com/vi/'+vId+'/'+resolution+'.jpg';
+
+        console.log(vId)
+    
+        chrome.downloads.download({
+          url: imageUrl,
+          filename: vId+'_'+resolution+'.jpg',
+          saveAs: true
+        }, function () {
+          if (chrome.runtime.lastError) {
+            console.error(chrome.runtime.lastError);
+          } else {
+            console.log('Image downloaded');
+          }
+        });
+      });
+   
+
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
         var activeTab = tabs[0];
        
