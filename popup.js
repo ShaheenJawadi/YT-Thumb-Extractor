9ed62cab12888472f3cbeb5ec667f8539fa5caca
shaheenj <jawadishaheen@gmail.com>
2023-12-25 05:45:17 +0100

    detect youtube

--- popup.js
+++ popup.js
@@ -1,5 +1,18 @@
 
 document.addEventListener('DOMContentLoaded',  function () {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        var activeTab = tabs[0];
+       
+        var url = new URL(activeTab.url);
+        var params = url.searchParams;
+        if (url.hostname === 'www.youtube.com' &&params.has('v') ) {
+      
+          console.log('This is youtube');
+        } else {
+         
+          console.log('This is not youtube');
+        }
+      });
 
 });
   
